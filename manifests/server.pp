6c2c6370ff6ac84945e716d6529b92e50fb39b1e
lennyi <lenny.ilyashov@wwt.com>
2017-01-23 16:01:29 -0600

    adds libarchive to server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -8,4 +8,16 @@ class pe_razor_complete::server {
   # Use the pe_razor module that's included with Puppet Enterprise
   include pe_razor
 
+  package { 'libarchive':
+    ensure => present,
+    before => Class['pe_razor'],
+  }
+
+  package { 'libarchive-devel-3.1.2-10.el7_2.x86_64.rpm':
+    ensure   => present,
+    provider => 'rpm',
+    source   => 'http://puppetfiles.wwt.com/libarchive-devel-3.1.2-10.el7_2.x86_64.rpm',
+    after    => Package['libarchive'],
+  }
+
 }
